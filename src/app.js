33fd68b32e0d0f1f659e0aa93508f31a023b3396
Raul-madero <127880270+Raul-madero@users.noreply.github.com>
2023-10-21 02:27:56 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = document.querySelector("#excuse");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function random(max) {
+    let result = Math.floor(Math.random() * max);
+    return result;
+  }
+  excuse.innerHTML = `${who[random(who.length)]} ${
+    action[random(action.length)]
+  } ${what[random(what.length)]} ${when[random(when.length)]}`;
 };
